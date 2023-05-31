330a96818a4950855a86b1c52dcbe02d977755e2
SaiBhanuBandi14114141 <105918956+SaiBhanuBandi14114141@users.noreply.github.com>
2023-05-31 12:59:30 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import Check from 'example/screens/check1';
+import * as React from 'react';
+
+import { View } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <Check />
+    </View>
+  );
+}
